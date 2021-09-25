10caeed1f1a78c5300a2d76febd5413f10da488f
RingoFloyd <65081992+RingoFloyd@users.noreply.github.com>
2021-09-25 22:34:44 +0200

    sass installed and working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import albums from "../albums";
 function App() {
     return (
         <div>
-            <h1>Testing</h1>
+            <h1>Reinie's Top 10 Rock Albums</h1>
             <ChartCard />
         </div>
     );
