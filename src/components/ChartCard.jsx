7c2e0cf099ee2096a67855d316c4718d298a22c3
RingoFloyd <65081992+RingoFloyd@users.noreply.github.com>
2021-09-26 23:04:54 +0200

    added grid display to card component

--- src/components/ChartCard.jsx
+++ src/components/ChartCard.jsx
@@ -3,11 +3,14 @@ import React from "react";
 function ChartCard(props) {
     return (
         <div className="card">
-            <div className="top">
+            <div className="info">
                 <h1 className="position">{props.position}</h1>
                 <h2 className="artistName">{props.artist}</h2>
                 <h2 className="title">{props.albumName}</h2>
-                <img className="cover-img" src={props.albumCover} alt="album_cover" />
+                
+            </div>
+            <div className="cover-img">
+            <img src={props.albumCover} alt="album_cover" />
             </div>
 
         </div>
